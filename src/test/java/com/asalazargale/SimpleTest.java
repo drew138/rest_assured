1ddf372d4931f64158dbcfe46d73515361115ca7
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2024-10-21 21:52:11 -0500

    feat: initial scaffolding

--- src/test/java/com/asalazargale/SimpleTest.java
+++ src/test/java/com/asalazargale/SimpleTest.java
@@ -1,9 +1,26 @@
 package com.asalazargale;
-import org.junit.jupyter.api.Test;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
 
 public class SimpleTest {
+    String url = "https:/simple-books-api.glitch.me";
     @Test
     public void sampleTest(){
+        String endpoint = "/orders";
+        String bearer = "";
+        String requestBody = "";
 
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .auth().oauth2(bearer)
+                .body(requestBody)
+                .when()
+                .post(url + endpoint)
+                .then()
+                .statusCode(201)
+                .extract().response();
     }
 }
