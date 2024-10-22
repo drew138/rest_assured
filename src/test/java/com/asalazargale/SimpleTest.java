b794d09876424a9faf5971c257dfe977e15723ce
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2024-10-21 22:04:23 -0500

    test: adds test to create user

--- src/test/java/com/asalazargale/SimpleTest.java
+++ src/test/java/com/asalazargale/SimpleTest.java
@@ -3,24 +3,33 @@ import org.junit.jupiter.api.Test;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.post;
 
 
 public class SimpleTest {
-    String url = "https:/simple-books-api.glitch.me";
+    String url = "https://demoqa.com";
     @Test
     public void sampleTest(){
-        String endpoint = "/orders";
+        String endpoint = "/Account/v1/User";
         String bearer = "";
-        String requestBody = "";
+        String requestBody = """
+        {
+            "userName": "aa12sdasdasdasdasda",
+            "password": "AGsaasd1asd$asdasd"
+        }
+        """;
+
 
         Response response = given()
                 .contentType(ContentType.JSON)
-                .auth().oauth2(bearer)
+//                .auth().oauth2(bearer)
                 .body(requestBody)
                 .when()
                 .post(url + endpoint)
                 .then()
                 .statusCode(201)
                 .extract().response();
+        post(url).then().statusCode(201);
+        response.body().prettyPrint();
     }
 }
